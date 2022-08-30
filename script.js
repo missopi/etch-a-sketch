3dcd35eb6b7a6f74b88cec91d22a89a3abe107cd
missopi <missopi@me.com>
2022-08-30 22:52:47 +0100

    Remove old flexbox grid code

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-
-//let rows = document.getElementsByClassName('row');
-//let columns = document.getElementsByClassName('column');
 //const clearBtn = document.getElementById('clear');
 //const blackBtn = document.getElementById('black');
 ////const colourBtn = document.getElementById('rgb');
@@ -23,32 +20,6 @@ function makeGrid(size) {
 
 makeGrid(16);
 
-
-// creates rows for grid
-//function makeRows(rowNumber) {
-   // for (i = 0; i < rowNumber; i++) {
-   //     let row = document.createElement("div");
-   //     grid.appendChild(row).className = "row";
-   //// };
-//};
-
-// creates columns for grid
-//function makeColumns(colNumber) {
-   // for (i = 0; i < rows.length; i++) {
-      //  for (j = 0; j < colNumber; j++) {
-         //   let newColumn = document.createElement("div");
-       //     rows[j].appendChild(newColumn).classList = "column";
-     //   };
-  //  };
-//};
-
-// creates a grid to a specific size 
-//function createGrid() {
-   // makeRows(16);
- //   makeColumns(16);
-//};
-
-//createGrid();
 
 //let currentMode = '';
 
